526080010a42eb35de453e5e74ca81e21811c694
mohammed elsedik rebbouh <rebbouh.mohammed.elseddik@gmail.com>
2022-05-07 10:36:36 +0100

    Add navBar

---
+++ src/components/navBar/NavBar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const NavBar = () => {
+    return (
+        <div className='navBar'>
+            <Link to='/books'>Books</Link>
+            <Link to='/categories'>Categories</Link>
+        </div>
+    )
+}
+
+
+export default NavBar;
