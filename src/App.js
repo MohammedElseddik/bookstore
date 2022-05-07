d97d9fa12ee07209e1b1314e0624bdca571af58c
mohammed elsedik rebbouh <rebbouh.mohammed.elseddik@gmail.com>
2022-05-07 12:05:45 +0100

    Add brand icom to the navbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import NavBar from './components/navBar/NavBar';
+import Books from './components/Books/Books';
 
 function App() {
   return (
@@ -8,10 +9,12 @@ function App() {
       <NavBar />
       <main>
         <Routes>
-          <Route path='/books' />
-          <Route path='/categories' />
+          <Route path='books' />
+          <Route path='books' />
+          <Route path='categories' />
         </Routes>
       </main>
+      <Books />
     </BrowserRouter>
   );
 }
--- src/components/navBar/NavBar.js
+++ src/components/navBar/NavBar.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom'
 const NavBar = () => {
     return (
         <div className='navBar'>
+            <Link to='/books'>Bookstore CMS</Link>
             <Link to='/books'>Books</Link>
             <Link to='/categories'>Categories</Link>
         </div>
