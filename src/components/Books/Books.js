2b4b827ac8c4b9a11898b073f431773211dabd37
mohammed elsedik rebbouh <rebbouh.mohammed.elseddik@gmail.com>
2022-05-20 11:35:01 +0100

    remove hard coded books from the Books React component

--- src/components/Books/Books.js
+++ src/components/Books/Books.js
@@ -1,41 +1,41 @@
-import React from 'react';
-import Book from '../Book/Book';
-import AddBookForm from '../AddBookForm/AddBookForm';
+import React from "react";
+//import Book from "../Book/Book";
+import AddBookForm from "../AddBookForm/AddBookForm";
 
 const Books = () => {
-  const books = [
-    {
-      id: 'book1',
-      title: 'The Hunger Games',
-      author: 'Suzanne Collins',
-      category: 'Action',
-    },
+  // const books = [
+  //   {
+  //     id: 'book1',
+  //     title: 'The Hunger Games',
+  //     author: 'Suzanne Collins',
+  //     category: 'Action',
+  //   },
 
-    {
-      id: 'book2',
-      title: 'Dune',
-      author: 'Frank Herbert',
-      category: 'Science Fiction',
-    },
+  //   {
+  //     id: 'book2',
+  //     title: 'Dune',
+  //     author: 'Frank Herbert',
+  //     category: 'Science Fiction',
+  //   },
 
-    {
-      id: 'book3',
-      title: 'Capital in the Twenty-First Century',
-      author: 'Suzanne Collins',
-      category: 'Economy',
-    },
-  ];
+  //   {
+  //     id: 'book3',
+  //     title: 'Capital in the Twenty-First Century',
+  //     author: 'Suzanne Collins',
+  //     category: 'Economy',
+  //   },
+  // ];
 
   return (
     <div className="Books">
-      {books.map((book) => (
+      {/* {books.map((book) => (
         <Book
           key={book.id}
           bookTitle={book.title}
           bookAuthor={book.author}
           bookCategory={book.category}
         />
-      ))}
+      ))} */}
       <AddBookForm />
     </div>
   );
