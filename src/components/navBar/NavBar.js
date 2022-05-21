40ca43cb9e8fd718a84caf3a55d82bc2941b1e28
mohammed elsedik rebbouh <rebbouh.mohammed.elseddik@gmail.com>
2022-05-21 20:17:11 +0100

    Style navigation bar

--- src/components/navBar/NavBar.js
+++ src/components/navBar/NavBar.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './NavBar.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const NavBar = () => (
-  <div className="navBar">
-    <Link to="/books">Bookstore CMS</Link>
-    <Link to="/books">Books</Link>
-    <Link to="/categories">Categories</Link>
-  </div>
+  <nav className="navBar">
+    <ul className="navBar__menu">
+      <li className="navBar__menu_item">
+        <Link to="/books" className="logo">
+          Bookstore CMS
+        </Link>
+      </li>
+      <li className="navBar__menu_item">
+        <Link to="/books">Books</Link>
+      </li>
+      <li className="navBar__menu_item">
+        <Link to="/categories">Categories</Link>
+      </li>
+    </ul>
+    <div className="user">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+        <path d="M224 256c70.7 0 128-57.31 128-128s-57.3-128-128-128C153.3 0 96 57.31 96 128S153.3 256 224 256zM274.7 304H173.3C77.61 304 0 381.6 0 477.3c0 19.14 15.52 34.67 34.66 34.67h378.7C432.5 512 448 496.5 448 477.3C448 381.6 370.4 304 274.7 304z" />
+      </svg>
+    </div>
+  </nav>
 );
 
 export default NavBar;
